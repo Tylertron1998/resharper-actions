8510c839985e6fb9a7146281b8cf8d67cea967d5
Tylertron <tgfdavis98@gmail.com>
2020-04-25 16:39:46 +0100

    fix toolcache error.

--- src/util/ToolCache.js
+++ src/util/ToolCache.js
@@ -1,5 +1,3 @@
-const DOWNLOAD_URL = `https://www.jetbrains.com/resharper/download/download-thanks.html?platform=${getCorrectPlatformString()}`;
-
 const toolcache = require('@actions/tool-cache');
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
@@ -9,7 +7,7 @@ const os = require('os');
 function getInspector() {
 	if (!toolcache.find('inspectcode')) {
 		core.debug('Downloading inspectcode...');
-		const downloadedPath = toolcache.downloadTool(DOWNLOAD_URL);
+		const downloadedPath = toolcache.downloadTool(`https://www.jetbrains.com/resharper/download/download-thanks.html?platform=${getCorrectPlatformString()}`);
 		const extractedFolder = toolcache.extractZip(downloadedPath);
 
 		const cachedPath = toolcache.cacheDir(extractedFolder);
