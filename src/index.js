2315b05f1a73fc9869e82f93442cf5e6792297a3
Tylertron <tgfdavis98@gmail.com>
2020-04-25 15:48:28 +0100

    remove fs-nextra as a dependency and add ncc for build.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const core = require('@actions/core');
 const Parser = require('./parser/Parser');
 const { runInspector } = require('./util/ToolCache');
-const fsn = require('fs-nextra');
+const fs = require('fs');
 
 const solutionDirectory = core.getInput('SLN_DIR');
 const failureLevel = core.getInput('FAIL_LEVEL');
 
 runInspector(solutionDirectory);
 
-fsn.readFile('./output.xml')
-	.then(contents => {
-		const parser = new Parser(contents, failureLevel);
-		parser.parse();
+const contents = fs.readFileSync('./output.xml', { encoding: 'utf8' });
+const parser = new Parser(contents, failureLevel);
 
-		if (parser.hasFailed) {
-			parser.showErrors();
-			core.setFailed('Resharper failed. Check log output.');
-		}
-	});
+parser.parse();
+
+if (parser.hasFailed) {
+	parser.showErrors();
+	core.setFailed('Resharper failed. Check log output.');
+}
