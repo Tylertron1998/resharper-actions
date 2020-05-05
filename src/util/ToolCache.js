cb646c8bba797a45cc3707189f5b786f40371aa2
Tyler Davis <tgfdavis98@gmail.com>
2020-05-05 04:44:45 +0100

    fix errors.

--- src/util/ToolCache.js
+++ src/util/ToolCache.js
@@ -17,19 +17,19 @@ async function getInspector() {
 
 		const url = downloadData.downloads[platform].link;
 
-		core.log('Cached Resharper Command Line Tools not found.');
-		core.log(`Downloading Resharper Command Line Tools from ${url}`);
+		core.info('Cached Resharper Command Line Tools not found.');
+		core.info(`Downloading Resharper Command Line Tools from ${url}`);
 		
 		const downloadedPath = await toolcache.downloadTool(url);
-		core.log(`Download Path: ${downloadedPath}`);
+		core.info(`Download Path: ${downloadedPath}`);
 		
 		const extractedFolder = await toolcache.extractZip(downloadedPath);
-		core.log(`Extracted Folder: ${extractedFolder}`);
+		core.info(`Extracted Folder: ${extractedFolder}`);
 		
 		inspectCodeDirectory = await toolcache.cacheDir(extractedFolder, "RSCLT", downloadData.version, platform);
-		core.log(`Cached Path: ${inspectCodeDirectory}`);
+		core.info(`Cached Path: ${inspectCodeDirectory}`);
 	} else {
-		core.log('Using cached Resharper Command Line Tools.');
+		core.info('Using cached Resharper Command Line Tools.');
 	}
 
 	core.addPath(inspectCodeDirectory);
